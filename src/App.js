b46150cac210a7a6f5874643e233ac492304c223
bobokandov <alisherbabakandov@gmail.com>
2023-01-27 16:01:08 +0500

    yuklash

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header className="shadow">
+        <div className="container-fluid py-3">
+          <img src="https://olcha.uz/_nuxt/logo-red.e48e0ab8.svg" alt="" />
+        </div>
+      </header>
+
+      <div id="products " className="py-5">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/266x266/products/2022-12-23/smartfon-xiaomi-redmi-note-11-pro-6-gb-128gb-zvezdno-goluboy-185214-0.jpeg"
+                  alt=""
+                />
+                <a href="#" className="groupName d-block mt-2 mb-3">
+                  Smartphone
+                </a>
+                <p className="price">2 290 000 so'm</p>
+                <p className="priceMonth">220 000 som/oy</p>
+                <p className="title"> Xiome Redmi 10 4/128GB Sea BLue</p>
+                <a href="#" className="btn">
+                  Sotvolish
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/266x266/products/2022-12-23/smartfon-xiaomi-redmi-note-11-pro-6-gb-128gb-zvezdno-goluboy-185214-0.jpeg"
+                  alt=""
+                />
+                <a href="#" className="groupName d-block mt-2 mb-3">
+                  Smartphone
+                </a>
+                <p className="price">2 290 000 so'm</p>
+                <p className="priceMonth">220 000 som/oy</p>
+                <p className="title"> Xiome Redmi 10 4/128GB Sea BLue</p>
+                <a href="#" className="btn">
+                  Sotvolish
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/266x266/products/2022-12-23/smartfon-xiaomi-redmi-note-11-pro-6-gb-128gb-zvezdno-goluboy-185214-0.jpeg"
+                  alt=""
+                />
+                <a href="#" className="groupName d-block mt-2 mb-3">
+                  Smartphone
+                </a>
+                <p className="price">2 290 000 so'm</p>
+                <p className="priceMonth">220 000 som/oy</p>
+                <p className="title"> Xiome Redmi 10 4/128GB Sea BLue</p>
+                <a href="#" className="btn">
+                  Sotvolish
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/266x266/products/2022-12-23/smartfon-xiaomi-redmi-note-11-pro-6-gb-128gb-zvezdno-goluboy-185214-0.jpeg"
+                  alt=""
+                />
+                <a href="#" className="groupName d-block mt-2 mb-3">
+                  Smartphone
+                </a>
+                <p className="price">2 290 000 so'm</p>
+                <p className="priceMonth">220 000 som/oy</p>
+                <p className="title"> Xiome Redmi 10 4/128GB Sea BLue</p>
+                <a href="#" className="btn">
+                  Sotvolish
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/266x266/products/2022-12-23/smartfon-xiaomi-redmi-note-11-pro-6-gb-128gb-zvezdno-goluboy-185214-0.jpeg"
+                  alt=""
+                />
+                <a href="#" className="groupName d-block mt-2 mb-3">
+                  Smartphone
+                </a>
+                <p className="price">2 290 000 so'm</p>
+                <p className="priceMonth">220 000 som/oy</p>
+                <p className="title"> Xiome Redmi 10 4/128GB Sea BLue</p>
+                <a href="#" className="btn">
+                  Sotvolish
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
